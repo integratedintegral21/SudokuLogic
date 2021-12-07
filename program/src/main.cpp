aa7d32a3adff6caf517de0c343f6716d451c6fd0
integratedintegral21 <w.boncela@gmail.com>
2021-12-07 14:12:59 +0000

    Implementing sudoku like a normal human

--- program/src/main.cpp
+++ program/src/main.cpp
@@ -75,17 +75,6 @@ int main(){
                     make_tuple(9,3,6),
             }
     };
-    /*
-    vector<vector<CellPos>> simpleConstraints = Utils::getSimpleConstraints();
-    Sudoku sudoku(simpleBoards[1]);
-    cout << sudoku.getBoardString() << endl;
-    time_t start, end;
-    time(&start);
-    Sudoku solvedSudoku = Utils::solveSudoku(sudoku);
-    time(&end);
-    cout << solvedSudoku.getBoardString() << endl;
-    cout << "Solved in " << fixed << double(end - start) << setprecision(5) << " sec" << endl;
-     */
     vector<CellPtr> cells = Utils::getCellsFromNumPoses(simpleBoards[0]);
     vector<CellVerifiers::CellGroup::SharedPtr> groups = Utils::getSimpleGroups(cells);
     Sudoku sudoku(cells, groups);
